41af792c0abee113abc60df4e9226dcdf31ef980
Nir <59836189+nirsaban@users.noreply.github.com>
2021-02-28 21:12:03 +0200

    update

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -41,9 +41,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 export default firebase;
